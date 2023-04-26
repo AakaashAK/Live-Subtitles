df0f0ac857ac3cfc94a70ff7ce497757e15b8a6c
AakaashAK <aakaashmamidala@gmail.com>
2023-04-26 12:51:36 +0530

    live

---
+++ Backend/shift.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.move('C:/Users/vikas/OneDrive/Desktop/Udaan proj/trial/video_Subtitled.mp4', 'C:/Users/vikas/OneDrive/Desktop/Udaan proj/myproj/assets/video_Subtitled.mp4')
+
